3eb1bc91f871252fe61abc5e8095ca9472f71aea
Abhishek Pal <abhishek1100861@gmail.com>
2022-01-08 01:26:19 +0530

    Home Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import logo from "../images/shared/logo.svg"
 const Navbar = () => {
     return (
-        <div>
-            Nav
-        </div>
+        <Nav>
+            <a href="{#}">
+            <img src={logo} alt="" />
+            </a>
+
+            <Menu>
+              <a href="{#}"> 00 Home </a>
+              <a href="{#}"> 01 Destination </a>
+              <a href="{#}"> 02 Crew  </a>
+              <a href="{#}"> 03 Technology </a>
+            </Menu>
+        </Nav>
     )
 }
+
+
+const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+ 
+    position: fixed;
+       top: 0;
+       left: 0;
+       right: 0;
+`
+
+const Menu = styled.div`
+    
+    width: 50%;
+    height: 10vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background: rgba( 208, 214, 249, 0.2 );
+  
+    backdrop-filter: blur( 5px );
+    -webkit-backdrop-filter: blur( 5px );
+    
+    a{
+        color: white;
+        text-transform: uppercase;
+        font-family: 'Barlow Condensed', sans-serif;
+    }
+`
 
 export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,15 +10,17 @@ const Home = () => {
                  
                   <h2>So you want to Travel </h2>
                   <h1>SPACE</h1>
-                  <p>Let’s face it; if you want to go to space, you might as well  genuinely go to outer space and not hover kind of on the <br /> edge of it. Well sit back, and relax because we’ll give you <br /> a truly out of this world experience!</p>
+                  <p>Let’s face it; if you want to go to space, you might as well  genuinely go to outer space and not hover kind of on the  edge of it. Well sit back, and relax because we’ll give you  a truly out of this world experience!</p>
                </Space>
                <Explore>
-                    <a href=""> Explore </a> 
+                    <a href="{#}"> Explore </a> 
                </Explore>
            </Section>
         </Container>
     )
 }
+
+
 
 const Container = styled.div`
     background-image: url(${bg});
@@ -34,30 +36,69 @@ const Section = styled.div`
     align-items: center;
     justify-content: space-around;
     color: white;
-   
+   padding-top: 40vh;
+
+   @media (max-width: 768px) {
+        flex-direction: column;
+        padding-top: 20vh;
+        justify-content: space-around;
+    }
     
 `
 const Space = styled.div`
     width: 50%;
+    color: #d0d6f9;
   h2{
       text-transform: uppercase;
+      font-weight: 300;
+     letter-spacing: 2px;
+     line-height: 1.6;
   }
     h1{
-        font-size: 60px;
+        font-size: 150px;
+        line-height: 1.5;
+        font-family: 'Bellefair', serif;
+        font-weight: 400;
+        color: white;
+    }
+    p{
+        line-height: 1.4;
+        font-size: 20px;
+    }
+
+    @media (max-width: 768px) {
+       width: 100%;
+       text-align: center;
+       h1{
+           font-size: 100px;
+       }
+       p{
+           width: 60%;
+           margin: 10px auto;
+       }
+    }
+
+    @media (max-width: 380px){
+        p{
+            width: 90%;
+        }
     }
 `
 const Explore = styled.div`
     width: 50%;
     font-size: 40px;
     background-color: white;
-    color: black;
     height: 250px;
     width: 250px;
     border-radius: 50%;
-    text-align: center;
     display: flex;
     justify-content: center;
     align-items: center;
     text-transform: uppercase;
+    
+    @media (max-width: 768px) {
+        margin-top: 80px;
+    }
+
 `
 export default Home
