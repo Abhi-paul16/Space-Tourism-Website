4f2ef11d50a5da299dd170702444969c879119c8
Abhishek Pal <abhishek1100861@gmail.com>
2022-01-21 23:44:30 +0530

    Crew Psge Structure

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 //images
-// import 
+import crewbg from '../images/crew/background-crew-desktop.jpg'
 import Douglas from '../images/crew/image-douglas-hurley.png'
 const Crew = () => {
     return (
@@ -12,6 +12,9 @@ const Crew = () => {
                <Heading>
                  <h2><span>02</span> Meet your crew</h2>
                  </Heading> 
+                 <div>
+
+
                  <PeopleDestination>
                      Commandar
                  </PeopleDestination>
@@ -21,16 +24,18 @@ const Crew = () => {
                  <PeopleDiscription>
                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur hic laboriosam qui quae voluptate inventore quam, incidunt, rem, numquam quia beatae fuga architecto. Quae cupiditate totam alias, provident odio rerum?
                  </PeopleDiscription>
+                 </div>
+                 <PeoplesLink>
+                    <div> <Link to='/'></Link> </div>
+                    <div> <Link to='/'></Link> </div>
+                    <div> <Link to='/'></Link> </div>
+                    <div> <Link to='/'></Link> </div>
+                 </PeoplesLink>
                  </AboutPeople>   
                  <PeopleView>
                      <img src={Douglas} alt="" />
                  </PeopleView>
-                 <PeoplesLink>
-                     <Link to='/'></Link>
-                     <Link to='/'></Link>
-                     <Link to='/'></Link>
-                     <Link to='/'></Link>
-                 </PeoplesLink>
+
                  </ContentWrapper>     
         </div>
     )
@@ -39,34 +44,58 @@ const Crew = () => {
 
 const ContentWrapper = styled.div`
     display: flex;
+    background-image: url(${crewbg});
 `
 
 const AboutPeople = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-around;
+    align-items: flex-start;
     height: 100vh;
-    background-image: url();
+    margin-left: 5rem;
+    color: white;
 `
 
 const Heading = styled.div`
-    
+    text-transform: uppercase;
 `
 
 const PeopleDestination = styled.div`
-    
+    text-transform: uppercase;
+    font-size: 2rem;
 `
 const PeopleName = styled.div`
-    
+    text-transform: uppercase;
+    font-size: 3rem ;
 `
 const PeopleDiscription = styled.div`
-    
+    width: 45%;
+    color: #d0d6f9;
 `
-const PeopleView = styled.div``
+const PeopleView = styled.div`
+    display: flex;
+    justify-content: end;
+    align-items: end;
+    img{
+        height: 80vh;
+    }
+`
 
 
 const PeoplesLink = styled.div`
+   display: flex;
+   width: 200px;
+   justify-content: space-around;
+    div{
+        height: 10px;
+        width:  10px;
+        border-radius: 50%;
+        background-color: #ffffff;
+        padding: 0.5rem;
+        cursor: pointer;
+        opacity: 0.2;
+    }
     
 `
 
